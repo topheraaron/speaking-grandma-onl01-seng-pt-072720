2ea43c7d9df67f7da1da79bbd64196c0df6e7032
Christopher Arias <chrstphr.aaron@gmail.com>
2020-07-30 00:12:30 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -5,7 +5,7 @@ def speak_to_grandma(phrase)
 # unless you shout it (type in all capitals).
     if phrase == "I LOVE YOU GRANDMA!"
       return "I LOVE YOU TOO PUMPKIN!"
-    elsif phrase == phrase.downcase
+    elsif phrase == phrase.upcase
       return "HUH?! SPEAK UP, SONNY!"
     elsif phrase == phrase.upcase
       return "NO, NOT SINCE 1938!"
