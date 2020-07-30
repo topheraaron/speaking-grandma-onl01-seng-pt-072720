2015d46083b22b1af907bf6d3b29ddb1d76fc009
Christopher Arias <chrstphr.aaron@gmail.com>
2020-07-30 00:17:03 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -8,7 +8,7 @@ def speak_to_grandma(phrase)
     elsif phrase == phrase.downcase
       return "HUH?! SPEAK UP, SONNY!"
     elsif phrase == phrase.upcase
-      return "NO, NOT SINCE 1938!"
+      return "HUH?! SPEAK UP, SONNY!"
     elsif phrase == phrase.downcase
       return "HUH?! SPEAK UP, SONNY!"
     else return "NO, NOT SINCE 1938!"
